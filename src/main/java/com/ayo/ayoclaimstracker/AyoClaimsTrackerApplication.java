4bb6f9202281d2eada080d665840d37f63c451e7
Marsha-S <marsha.saminathan@ayo4u.com>
2022-11-08 23:02:58 +0200

    scan

--- src/main/java/com/ayo/ayoclaimstracker/AyoClaimsTrackerApplication.java
+++ src/main/java/com/ayo/ayoclaimstracker/AyoClaimsTrackerApplication.java
@@ -2,8 +2,10 @@ package com.ayo.ayoclaimstracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"src/main/java/com/ayo/ayoclaimstracker/controllers"})
 public class AyoClaimsTrackerApplication {
 
 	public static void main(String[] args) {
